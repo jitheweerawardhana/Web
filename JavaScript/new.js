2568a9817ae27ee6c16e7b3f4200ab97965193d9
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-10-05 17:03:46 +0530

    Create new.js

---
+++ JavaScript/new.js
@@ -0,0 +1,7 @@
+function onMouseOverEvent() {
+    document.getElementById("mouseOver").src = "chat-c.png"; 
+}
+
+function onMouseOutEvent() {
+    document.getElementById("mouseOver").src = "chat-c-1.png"; 
+}
